799682d6d6fd0f169de5015d6babc6717459ceb4
GillAkash2232 <akashgill253361@gmail.com>
2024-04-21 00:37:36 +0530

    feat: Created Navbar

---
+++ nom-nom-nook/src/pages/User/Landing.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar";
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function Landing() {
+    return (
+        <div className="Landing">
+            <Navbar />
+        </div>
+    );
+}
+
+export default Landing;
