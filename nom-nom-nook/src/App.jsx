4d2adbca6f8d5858f7adaa69f4d9f58694792351
GurpreetSarangal <gurpreetsarangal7@gmail.com>
2024-04-21 07:43:45 +0530

    feat: created login signup component

--- nom-nom-nook/src/App.jsx
+++ nom-nom-nook/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Routes, Route} from  "react-router-dom";
-import Landing from './pages/User/Landing';
+import Landing from './pages/User/Landing'
+import Signup from './pages/User/Signup'
+import Login from './pages/User/Login';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
