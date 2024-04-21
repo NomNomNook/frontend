80314981c373402bc9be7aab0d257dd4be324a1d
GillAkash2232 <160337190+GillAkash2232@users.noreply.github.com>
2024-04-21 05:50:27 +0530

    Feat landing page (#5)
    * feat: Created Navbar
    
    * feat: Created landing page
    
    * feat: created pricing page

--- nom-nom-nook/src/App.jsx
+++ nom-nom-nook/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Routes, Route} from  "react-router-dom";
 import Landing from './pages/User/Landing';
+import Pricing from './pages/User/Pricing';
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/pricing" element={<Pricing />} />
           
         </Routes>
       </BrowserRouter>
